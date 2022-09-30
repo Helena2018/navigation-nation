e1248c8e4fc197c64b2ded2bffc5d2b7f2017e5d
Helena2018 <helena2020liu@gmail.com>
2022-09-30 14:00:55 +1300

    get elements in JS

---
+++ script.js
@@ -0,0 +1,9 @@
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
+const menuBars = document.getElementById('menu-bars');
+
+const navItems = [nav1, nav2, nav3, nav4, nav5];
