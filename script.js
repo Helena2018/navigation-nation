04eaca62030987c1a6c400dec2c2af0d079fa9ef
Helena2018 <helena2020liu@gmail.com>
2022-09-30 16:59:12 +1300

    created navAnimation and toggleNav function

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const nav5 = document.getElementById('nav-5');
 const menuBars = document.getElementById('menu-bars');
 
 const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+function navAnimation(direction1, direction2) {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`)
+  })
+}
+
+function toggleNav() {
+  overlay.classList.toggle('overlay-active');
+  if(overlay.classList.contains('overlay-active')) {
+    overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+
+  }
+
+}
+
+menuBars.addEventListener('click', toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener('click', toggleNav)
+})
